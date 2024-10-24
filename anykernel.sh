d8007cbc0e722d967c55d70a525d643c04e7770e
K R I S H N A <krishnakripa34567@gmail.com>
2024-10-24 18:04:27 +0530

    init Vantom Kernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Sleepy Kernel for Redmi Note 10 Pro & Pro Max by t.me/itsshashanksp
+kernel.string=Vantom Kernel for Redmi Note 10 Pro & Pro Max
 do.devicecheck=1
 do.modules=0
 do.systemless=1
